a131ab984727a209eb8756fb23337d103ceb22c1
Squid <squid@stormwind.it>
2012-08-20 17:56:31 +0200

    Added simple login activity (grabbed from this example http://www.androidhive.info/2011/10/android-login-and-registration-screen-design/)
    Added AQuery library

---
+++ src/main/java/it/vivido/aurora/client/auroraclient/LoginActivity.java
@@ -0,0 +1,40 @@
+package it.vivido.aurora.client.auroraclient;
+
+import com.androidquery.AQuery;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+
+	private static String TAG = "aurora_loginactivity";
+
+	private AQuery aq;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		Log.i(TAG, "onCreate");
+		setContentView(R.layout.main);
+		aq = new AQuery(this);
+
+		
+	
+		aq.id(R.id.btnLogin).text("Login?").clicked(this, "login");
+
+		
+		
+	}
+
+	public void login(View v)
+	{
+		Toast.makeText(this, "Login in corso...", Toast.LENGTH_LONG).show();
+
+	}
+
+}
+
